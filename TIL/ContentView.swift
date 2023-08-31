62ddb7c962a3a04a35ff21f93b15e664081a5ce1
silver-zoo <okzk123@gmail.com>
2023-08-31 22:34:06 +0900

    커밋1. 변화 없음(커밋 연습용)

--- TIL/ContentView.swift
+++ TIL/ContentView.swift
@@ -10,6 +10,7 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
+    // Do Somting
 
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
